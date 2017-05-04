3820dcb6a8e02a4234b9485f42c7e19ae79bd823
PoornimaNagendran <PoornimaNagendran@users.noreply.github.com>
2017-05-04 13:35:42 +0530

    ASCII
    ASCII

---
+++ ASCII.java
@@ -0,0 +1,13 @@
+
+public class ASCII {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ASCII value");
+for(int i=0;i<128;i++)
+{
+	System.out.println((char)i+" - "+i);
+}
+	}
+
+}
